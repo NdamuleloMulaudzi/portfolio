22f96e94951c07a3b794c774cf775836113fef8e
NdamuleloMulaudzi <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2025-03-20 15:56:14 +0200

    add hoem rings and socials icons

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,11 +1,44 @@
 import React from 'react'
 import '../styles/Home.css'
+import Image from 'next/image'
 import Navbar from '../shared/Navbar'
 
 function Home() {
   return (
     <div className='home-container h-[100vh] w-[100vw] text-white'>
        <Navbar />
+
+       <div className='circles'>
+          <div className="circle one"></div>
+          <div className="circle two"></div>
+          <div className="circle three"></div>
+          <div className="circle four"></div>
+       </div>
+
+
+       <div className='socials'>
+          <div className="social">
+            <div className="line one"></div>
+            <Image  className="icons absolute top-[36%] right-[330px] mt-[160px]"  
+            src="/icons/x.svg" alt="X (Twitter) Icon" width={55} height={55} />
+          </div>
+
+          <div className="social">
+            <div className="line two"></div>
+            <Image className="icons absolute top-[36%] right-[285px] mt-[90px]"  src="/icons/github.svg" alt="GitHub Icon" width={60} height={60} />
+          </div>
+
+          <div className="social">
+            <div className="line three"></div>
+            <Image className="icons absolute top-[39%] right-[242px] mt-[3px] "  src="/icons/linkedin.svg" alt="LinkedIn Icon" width={55} height={55} />
+          </div>
+       </div>
+
+      <div className="hello">
+        <p>Hello</p>
+        <div className='orange-line'></div>
+      </div>
+
     </div>
   )
 }
--- src/app/shared/Navbar.tsx
+++ src/app/shared/Navbar.tsx
@@ -3,15 +3,18 @@ import '../styles/Navbar.css'
 
 function Navbar() {
   return (
-    <div>
-        <div className='px-8 py-9'>
+    <div className='flex justify-between'>
+        <div className='px-8 py-15 flex items-center gap-5'>
             <p className='header-logo text-[40px]'>NDAMU</p>
-            <div className='bg-[#211059]'></div>
+            <div className='bg-[#CE5A5A] border w-[20px] h-[20px] rounded-[16px]'></div>
         </div>
 
-        <div>
-
-        </div>
+        <div className='flex gap-9 px-15 py-15 text-[20px] font-bold'>
+            <a href="about">ABOUT</a>
+            <a href="projects">PROJECTS</a>
+            <a href="experience">EXPERIENCE</a>
+            <a href="contact">CONTACT</a>
+        </div>   
 
     </div>
   )
